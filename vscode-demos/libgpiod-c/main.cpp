c3829fce370142799ccbc527377c7b2902f3d4a5
Ken sloat <ken.s@variscite.com>
2024-01-18 15:00:07 -0500

    vscode-demos: libgpiod-c: Add multi-threaded button support to demo
    Add button framework support and apply to SOM_MX8M_MINI platform alone
    for now. This uses another thread to read any of the push buttons on the
    symphony board and increments the count value. The value is normally
    decremented by the LED blinking routine, so pressing the button extends
    the lifetime of the application run.
    
    Signed-off-by: Ken sloat <ken.s@variscite.com>

--- vscode-demos/libgpiod-c/main.cpp
+++ vscode-demos/libgpiod-c/main.cpp
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <linux/input.h>
+#include <fcntl.h>
 
 #ifndef    CONSUMER
 #define    CONSUMER    "Variscite"
@@ -22,6 +25,11 @@ typedef struct platform_data_t
 {
     const char *gpio_chip_name;
     unsigned int gpio_line_num;
+    bool has_btn;
+    const char *btn_input_dev;
+    int btn_fd;
+    pthread_mutex_t btn_mutex;
+    unsigned int blink_val;
 } platform_data_t;
 
 /**
@@ -52,6 +60,68 @@ som_t detect_som() {
    return som;
 }
 
+unsigned int get_blink_count(platform_data_t *platform)
+{
+    unsigned int ret;
+    if (platform->has_btn) {
+        pthread_mutex_lock(&platform->btn_mutex);
+        ret = platform->blink_val;
+        pthread_mutex_unlock(&platform->btn_mutex);
+    } else {
+        ret = platform->blink_val;
+    }
+
+    return ret;
+}
+
+void set_blink_count(platform_data_t *platform, unsigned int val)
+{
+    if (platform->has_btn) {
+        pthread_mutex_lock(&platform->btn_mutex);
+        platform->blink_val = val;
+        pthread_mutex_unlock(&platform->btn_mutex);
+    } else {
+        platform->blink_val = val;
+    }
+}
+
+void inc_blink_count(platform_data_t *platform)
+{
+    unsigned int count;
+
+    count = get_blink_count(platform);
+    count++;
+    set_blink_count(platform, count);
+}
+
+void dec_blink_count(platform_data_t *platform)
+{
+    unsigned int count;
+
+    count = get_blink_count(platform);
+    if (count) {
+        count--;
+    }
+    set_blink_count(platform, count);
+}
+
+void *btn_read_thread(void *data) {
+    platform_data_t *platform = (platform_data_t *)data;
+    struct input_event event;
+    size_t bytes;
+    while(1) {
+        bytes = read(platform->btn_fd, &event, sizeof(struct input_event));
+        if (bytes) {
+            if (event.type == EV_KEY) {
+                /* On press */
+                if (event.value == 0) {
+                    /* Works with any button */
+                    inc_blink_count(platform);
+                }
+            }
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -59,9 +129,8 @@ int main(int argc, char **argv)
     volatile unsigned int i, ret, val;
     struct gpiod_chip *chip;
     struct gpiod_line *line;
-    const char * chipname = 0;
-    unsigned int line_num;
     platform_data_t platform;
+    pthread_t btn_thread;
 
     memset(&platform, 0, sizeof(platform_data_t));
 
@@ -75,6 +144,8 @@ int main(int argc, char **argv)
             /* VAR-SOM-MX8M-MINI Symphony Custom Board I2C GPIO #0 */
             platform.gpio_chip_name = "gpiochip5";
             platform.gpio_line_num = 0;
+            platform.btn_input_dev = "/dev/input/event1";
+            platform.has_btn = true;
         break;
         /* Todo: Add other SoMs */
         default:
@@ -103,17 +174,47 @@ int main(int argc, char **argv)
         goto release_line;
     }
 
-    /* Blink 5 times */
+    /* Open button input device and spawn thread if applicable */
+    if (platform.has_btn) {
+        platform.btn_fd = open(platform.btn_input_dev, O_RDONLY);
+        if (platform.btn_fd < 0) {
+            printf("Error: Unable to open %s\n", platform.btn_input_dev);
+            goto release_line;
+        }
+        if (pthread_mutex_init(&platform.btn_mutex, NULL)) {
+            printf("Error: Unable to init mutex\n");
+            goto release_line;
+        }
+
+        if (pthread_create(&btn_thread, NULL, btn_read_thread, &platform)) {
+            printf("Error: Unable to create thread\n");
+            goto release_line;
+        }
+    }
+
+
+    /* Toggle LED GPIO x10 times */
+    set_blink_count(&platform, 10);
     val = 0;
-    for (i = 0; i < 5; i++) {
+    while(get_blink_count(&platform))
+    {
         ret = gpiod_line_set_value(line, val);
         if (ret < 0) {
             perror("Set line output failed\n");
             goto release_line;
         }
-        printf("Output %u on line #%u\n", val, line_num);
-        sleep(1);
+        printf("blink count: %d output_val: %d\n", get_blink_count(&platform), val);
+
+        usleep(500000);
         val = !val;
+
+        dec_blink_count(&platform);
+    }
+    printf("Count expired, exiting...\n");
+
+    if (platform.has_btn) {
+        pthread_cancel(btn_thread);
+        pthread_join(btn_thread, NULL);
     }
 
 release_line:
